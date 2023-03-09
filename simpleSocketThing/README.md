716ebd703d4dc67f25ef2257e6656f216db91abe
aaron <aaron.chakerian@students.mq.edu.au>
2023-03-09 19:09:30 +1100

    week3 prac completed

--- simpleSocketThing/README.md
+++ simpleSocketThing/README.md
@@ -1,8 +1,2 @@
 COMP3100 Practical WK3 completed.
-<<<<<<< HEAD
-GIT BRANCHING
 
-=======
-
-Putting onto main -> branch2
->>>>>>> branch2
--- simpleSocketThing/src/Client.java
+++ simpleSocketThing/src/Client.java
@@ -3,6 +3,8 @@ import java.net.*;
 
 public class Client {
     public static void main(String[] args) {
+        
+        
         try {
             
             Socket s = new Socket("localhost", 50000);
